118d326f819a09a55433896fb9e143808b824275
Alan Osys <72283731+AlanOsys@users.noreply.github.com>
2026-07-31 11:53:28 +0100

    Update pythonNiftyScript.py

--- pythonNiftyScript.py
+++ pythonNiftyScript.py
@@ -22,8 +22,8 @@ import pydicom
 logging.basicConfig(level=logging.INFO)
 print(nipet.gpuinfo())
 # get all the scanner parameters
-mMRpars = nipet.get_mmrparams('/store/mmr_hardwareumaps/')
-folderin = "/store/amyloid_brain"
+mMRpars = nipet.get_mmrparams('/home/di0/Downloads/store/mmr_hardwareumaps-v-hdr/')
+folderin = "/home/di0/Downloads/amyloidPET_FBP_TP0"
 folderout = "."  # realtive to `{folderin}/niftyout`
 itr = 7  # number of iterations (will be multiplied by 14 for MLEM)
 fwhm = 2.5  # mm (for resolution modelling)
@@ -39,7 +39,7 @@ datain = nipet.classify_input(folderin, mMRpars, recurse=-1)
 opth = path.join(datain['corepath'], "niftyout")
 
 datain
-
+mMRpars['Cnt']['HMUDIR'] = '/home/di0/Downloads/store/mmr_hardwareumaps-v-hdr/'
 # hardware mu-map (bed, head/neck coils)
 mu_h = nipet.hdw_mumap(datain, [1,2,4], mMRpars, outpath=opth, use_stored=True)
 
@@ -50,58 +50,13 @@ mMRpars['Cnt']['BTP'] = 0
 m = nipet.mmrhist(datain, mMRpars, outpath=opth, store=True, use_stored=True)
 hst = m
 
-
-
 import nibabel as nib
 import os
 import numpy
 from miutil.plot import apply_cmap, imscroll
 import cuvec as cu
-def gaussian_highpass(shape, sigma):
-    import numpy as np
 
-    # --- Parameters ---
-    size = shape  # Size of the square array (e.g., 20x20)
-    radius = 40 # Radius of the circle
-    center_x, center_y = size[1] // 2, size[2]// 2 # Center of the array
-
-    # --- Create coordinate grids ---
-    # Generate sequences from 0 to size-1 for x and y
-    x = np.arange(size[1])
-    y = np.arange(size[2])
-    # Create a 2D grid of coordinates
-    xv, yv = np.meshgrid(x, y)
-
-    # --- Calculate distances from the center ---
-    # (xv - center_x)**2 + (yv - center_y)**2 gives squared distance
-    # Apply the condition for points inside the circle
-    mask = (xv - center_x)**2 + (yv - center_y)**2 < radius**2
-
-    # --- Create the array ---
-    # Start with an array of zeros
-    circle_array = np.zeros((size[2], size[1]), dtype=int)
-    # Use the boolean mask to set values to 1 where the condition is True
-    circle_array[mask] = 1
-    p = []
-    for s in range(size[0]):
-        p.append(circle_array)
-    #print(circle_array)
-    return p
-cylinder = gaussian_highpass((837,252,344),30)
-cuCylinder = cu.asarray(cylinder,dtype=np.float32)
-#imscroll(cuCylinder)
-#recon = nipet.alanForward(cuCylinder.T,[mu_h['im'],mu_o['im']],mMRpars)
-txLUT = mMRpars['txLUT']
-axLUT = mMRpars['axLUT']
 Cnt = mMRpars['Cnt']
-Dir = "~/QUADRA_HC/Imaging Data/QUADRA_HC_001/Test/"
-file = nib.load(Dir+"/QUADRA_HC_001_Test_PT-SUV.nii.gz")
-img = file.get_fdata()
-#nipet.alanForward(cu.asarray(img[0],dtype=np.float32),[mu_h['im'],mu_o['im']],mMRpars)
-NRNG_c = Cnt['RNG_END'] - Cnt['RNG_STRT']
-# number of sinos in span-1
-nsinos = NRNG_c**2
-out_shape = txLUT['Naw'], nsinos
 sinog = cu.zeros((127,344,344),dtype=np.float32)#cu.zeros(out_shape, dtype=np.float32)
 import numpy as np
 
@@ -135,10 +90,113 @@ im = cu.asarray(cylinder,dtype=np.float32)
 #nip.prj.petprj.fprj(sinog, im, txLUT, axLUT, isub, Cnt,1)
 
 A = nipet.frwd_prj(im, mMRpars, attenuation=False)
-#r = nipet.randoms(A, mMRpars)[0] 
-B = nipet.back_prj(A, mMRpars)
-print(np.max(B))
-h = 0.5 * np.max(B)
-#randoms
-r = nipet.randoms(m,mMRpars)[0]
-s = nipet.vsm(datain, (mu_h['im'], mu_o['im']), B, mMRpars, m, r)
+
+from niftypet.nipet.prj import alanrec
+muMaps = (mu_h['im'],mu_o['im'])
+fwd = alanrec.Forward(im,(mu_h['im'],mu_o['im']),mMRpars,m)[1]
+rands = alanrec.Randoms(mMRpars,hst)[0]
+eim = alanrec.Scatter_EMML(m['psino'],datain,(mu_h['im'],mu_o['im']),mMRpars,m,1)
+#imscroll(eim)
+#scatter = alanrec.Scatter(datain, muMaps, mMRpars,hst,rands,eim)
+#recon = alanrec.Scatter_EMML(m['psino'],datain,(mu_h['im'],mu_o['im']),mMRpars,m,10)
+#recon2 = alanrec.Claude_OSEM(m['psino'],datain,(mu_h['im'],mu_o['im']),mMRpars,m,4,6)
+recon3 = np.asarray(alanrec.Scatter_BSREM_Updated(m['psino'],datain,(mu_h['im'],mu_o['im']),mMRpars,m,5,14,rands,beta=10))
+imscroll((recon3))
+#scatter = alanrec.Scatter(datain, muMaps, mMRpars,hst,rands,eim)
+#recon5 = alanrec.Scatter_EMML(m['psino'],datain,(mu_h['im'],mu_o['im']),mMRpars,m,10,rands,scatter)
+recon2 = nipet.mmrchain(datain, mMRpars,itr=5,histo=m,mu_h=mu_h,mu_o=mu_o,psf=2.5,recmod=1,outpath=None,fcomment='',store_img=True)
+#imscroll(recon2['im'])
+diff = recon3-recon2['im']
+#imscroll(diff,cmap="magma")
+#mask = alanrec.create_brain_mask(recon3,15.0,10.0)
+#brain = alanrec.apply_brain_mask(recon3, mask)
+#imscroll(brain)
+#msk = mmrimg.get_cylinder(Cnt, rad=11.5, xo=0, yo=1, unival=1, gpu_dim=False) > 0.9
+#recon3[~msk] = 0.0
+#recon2['im'][~msk] = 0.0
+#diff[~msk] = 0.0
+from niftypet.nipet import resources
+ncmp,rest,res = alanrec.getNormParameters(datain,Cnt)
+tref = hst['t0']
+lmbd = np.log(2) / resources.riLUT[Cnt['ISOTOPE']]['thalf']
+dcycrr = np.exp(lmbd * tref) * lmbd * hst['dur'] / (1 - np.exp(-lmbd * hst['dur']))
+# apply quantitative correction to the image
+qf = ncmp['qf'] / resources.riLUT[Cnt['ISOTOPE']]['BF'] / float(hst['dur'])
+qf_loc = ncmp['qf_loc']
+recon3Adjusted = recon3*qf*dcycrr*qf_loc #recon2['im']
+reconAgainst = recon2['im']#recon3*qf*dcycrr*qf_loc
+import matplotlib.pyplot as plt
+
+fig, axes = plt.subplots(nrows=1, ncols=3)
+
+slide = 50
+im1 = axes[0].imshow(reconAgainst[slide], cmap='inferno')
+im2 = axes[1].imshow(recon3Adjusted[slide], cmap='inferno')
+im3 = axes[2].imshow((recon3Adjusted-reconAgainst)[slide], cmap='bwr')
+
+
+fig.colorbar(im1, ax=axes[0])
+fig.colorbar(im2, ax=axes[1])
+fig.colorbar(im3, ax=axes[2])
+
+np.sqrt(np.mean((recon3Adjusted - reconAgainst)) ** 2)#
+
+
+
+
+# def normalize_array(arr):
+#     norm_arr = (arr - np.min(arr)) / (np.max(arr) - np.min(arr))
+#     return norm_arr
+# noise1 = []
+# noise2 = []
+# noise3 = []
+# recon1 = np.array(alanrec.Scatter_EMML(m['psino'],datain,(mu_h['im'],mu_o['im']),mMRpars,m,42,rands))
+# for i in recon1:
+#     noise1.append(alanrec.NoiseEstim(i,6))
+# noise1 = normalize_array(noise1)
+
+# for i in recon2['im']:
+#     noise2.append(alanrec.NoiseEstim(i,6))
+    # noise2 = normalize_array(noise2)
+    # for i in recon3:
+#     noise3.append(alanrec.NoiseEstim(i,6))
+# noise3 = normalize_array(noise3)
+# plt.plot(noise1)
+# plt.plot(noise2)
+# plt.plot(noise3)
+# plt.legend(['EMML','True OSEM','My OSEM'])
+# add space for colour bar
+
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(6, 2.5),
+                         gridspec_kw={'wspace': 0.05})
+slide = 45
+
+# Get image dimensions and calculate crop bounds
+h, w = reconAgainst[slide].shape
+cy, cx = h // 2, w // 2
+half = 70  # 30x30 crop → 15 pixels each side
+
+# Crop each image
+crop_against = reconAgainst[slide][cy-half:cy+half, cx-half:cx+half]
+crop_adjusted = recon3Adjusted[slide][cy-half:cy+half, cx-half:cx+half]
+crop_diff = (recon3Adjusted - reconAgainst)[slide][cy-half:cy+half, cx-half:cx+half]
+
+im1 = axes[0].imshow(crop_against, cmap='hot')
+im2 = axes[1].imshow(crop_adjusted, cmap='hot')
+im3 = axes[2].imshow(crop_diff, cmap='bwr')
+
+fig.colorbar(im1, ax=axes[0])
+fig.colorbar(im2, ax=axes[1])
+fig.colorbar(im3, ax=axes[2])
+
+axes[0].set_title('OSEM')
+axes[1].set_title('BSREM')
+axes[2].set_title('Difference')
+
+plt.tight_layout()
+plt.show()
